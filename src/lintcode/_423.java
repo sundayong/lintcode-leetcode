51bd6e2c2664b3f29f0c0d70603c49e2c6795350
sundayong <1137353877@qq.com>
2019-05-06 18:07:09 +0800

    423

--- src/lintcode/_423.java
+++ src/lintcode/_423.java
@@ -1,7 +1,30 @@
 package lintcode;
 
 public class _423 {
-    public static void main(String[] args) {
 
+    public static boolean isValidParentheses(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        int num = s.length();
+        while (num > 0) {
+            s = s.replaceAll("\\(\\)", "").replaceAll("\\{\\}", "")
+                    .replaceAll("\\[\\]", "");
+            num--;
+            if (s.length() == 0) {
+                break;
+            }
+        }
+        if (s.length() != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}{}";
+//        System.out.println(s.replaceAll("\\{\\}", ""));
+        System.out.println(isValidParentheses(s));
     }
 }
