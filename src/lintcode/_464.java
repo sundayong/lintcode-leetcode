e7e622a7d9e73b471d0f6427639c3710d35c2e4b
sundayong <1137353877@qq.com>
2019-05-07 18:49:49 +0800

    464 最骚的方式

---
+++ src/lintcode/_464.java
@@ -0,0 +1,16 @@
+package lintcode;
+
+import java.util.Arrays;
+
+public class _464 {
+
+
+    public static void sortIntegers2(int[] A) {
+        Arrays.sort(A);
+    }
+
+    public static void main(String[] args) {
+        int[] A = {3, 2, 1, 4, 5};
+        sortIntegers2(A);
+    }
+}
